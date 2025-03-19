6b465795942b6e241e9e724a46cb530006bbf503
LagadGlaz <baptistefougeray2@gmail.fr>
2025-03-19 01:30:36 +0100

    Version 0.0.6 / Frist_Base

---
+++ Data_Base/init.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Joueurs (
+                        joueur_id INT AUTO_INCREMENT PRIMARY KEY,
+                        joueur_nom VARCHAR(50),
+
+                        nombre_partie INT DEFAULT 0,
+                        score_total INT DEFAULT 0,
+                        ratio_score FLOAT DEFAULT 0,
+                        ratio_rang FLOAT DEFAULT 0,
+                        elo FLOAT DEFAULT 500,
+);
+
+CREATE TABLE Partie (
+                        partie_id INT PRIMARY KEY,
+
+                        rang int,
+                        nombre_joueur INT,
+
+                        joueur_id INT,
+                        joueur_nom VARCHAR(50),
+                        joueur_score INT,
+
+                        date_partie DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+                        FOREIGN KEY (joueur_id) REFERENCES Joueurs(joueur_id) ON DELETE CASCADE,
+);
+
+CREATE TRIGGER after_insert_partie
+AFTER INSERT ON Partie
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK; -- Annule toutes les opérations si une erreur survient
+    END;
+
+    START TRANSACTION;
+        UPDATE Joueurs SET nombre_partie = nombre_partie + 1 WHERE joueur_id = NEW.joueur_id;
+        UPDATE Joueurs SET Joueurs.score_total = score_total  + NEW.joueur_score WHERE joueur_id = NEW.joueur_id;
+        UPDATE Joueurs SET ration_score = IF(nombre_partie > 0, score_total / nombre_partie, 0) WHERE joueur_id = NEW.joueur_id;
+        UPDATE Joueurs SET Joueurs.ratio_rang  = IF(nombre_partie > 0, (ratio_rang * (nombre_partie -1) + NEW.rang / NEW.nombre_joueur) / nombre_partie, 0) WHERE joueur_id = NEW.joueur_id;
+    COMMIT;
+END
+
+CREATE TRIGGER after_delete_partie
+AFTER DELETE ON Partie
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+
+    BEGIN
+        ROLLBACK; -- Annule toutes les opérations si une erreur survient
+    END;
+
+    START TRANSACTION;
+        UPDATE Joueurs SET nombre_partie = nombre_partie - 1 WHERE joueur_id = OLD.joueur_id;
+        UPDATE Joueurs SET Joueurs.score_total = score_total  - OLD.joueur_score WHERE joueur_id = OLD.joueur_id;
+        UPDATE Joueurs SET ration_score = IF(nombre_partie > 0, score_total / nombre_partie, 0) WHERE joueur_id = OLD.joueur_id;
+        UPDATE Joueurs SET Joueurs.ratio_rang  = 1 WHERE joueur_id = OLD.joueur_id;
+    COMMIT;
+END
